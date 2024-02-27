8931641d21370fd28000f21fd8e41c6d0d5520e0
zywicki-314 <144706181+zywicki-314@users.noreply.github.com>
2024-02-27 10:17:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-const slider = document.querySelectorAll('.slider');
+const slider = document.querySelector('div.slider');
+const slidesColection = document.querySelector('div.slides-wrapper')
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
 const dotContainer = document.querySelector('.dots')
+let currentSlide = 0;
 
 // ==============================================
 
@@ -12,23 +14,68 @@ const boxOfCategorys =  document.querySelector("div.cat-filteredCat");
 const catArr = ['swetry', 'skarpety', 'kocyki', 'zima-on', 'zima-ona', 'zima-kids']
 let activeSlides = [...allItems];
 let testActiveSlides = [];
-let SETtestActiveSlides = []
 let ostatniPoziomFiltracjiFoto = [];
 let SETostatniPoziomFiltracjiFoto = [];
 let odfiltrowaneKlucze = [];
 let aktywneKlucze = [];
 let SETwidoczneKategorie = [];
-let widoczneKategorie = [];
-    
+let widoczneKategorie = [...allCategorys];
+
+// Dodanie-kasowanie klasy 'display-none' ze wszystkich slidów
+
+const hideAllSlides = function(){
+    allItems.forEach(i=>{
+        i.classList.add('display-none')
+    })
+}
+
+const showAllSlides = function(){
+    allItems.forEach(i=>{
+        i.classList.remove('display-none')
+    })
+}
+
+//  kasowanie klasy 'display-none' ze wszystkich aktywnych slidów
+const showActiveSlides = function(){
+    activeSlides.forEach(s=>{
+        s.classList.remove('display-none')
+    })
+}
+
+// Dodanie-kasowanie klasy 'display-none' ze wszystkich kategorii
+
+const hideAllCategories = function(){
+    allCategorys.forEach(c=>{
+        c.classList.add('display-none')
+    })
+}
+const showAllCategories = function(){
+    allCategorys.forEach(c=>{
+        c.classList.remove('display-none')
+    })    
+}
+// Kasowanie klasy 'category-item__On-Click' ze wszystkich kategorii
+const hideAllActiveCategories = function(){
+    allCategorys.forEach(c=>{
+        c.classList.remove('category-item__On-Click')
+    }) 
+}
+
+// 
+   
 const inPutSelect = function(){
-    selectElement.addEventListener("change", (event) => {    
+    currentSlide = 0
+    selectElement.addEventListener("change", (event) => {
+        event.preventDefault()
+        event.stopPropagation()
         const selectedOption = event.target.value;
-        SETtestActiveSlides = []
         testActiveSlides = []
+        hideAllActiveCategories()
         pierwszyEtapFiltracji(selectedOption)  
         filtracjaPoWybranuiKategorii()
         wubranyInPutWszystko(selectedOption)
         creatDotsForActiveSlides(activeSlides)
+        activateCurrentDot(currentSlide)
         return activeSlides
     });
 }
@@ -39,32 +86,34 @@ const pierwszyEtapFiltracji = function(selectedOption){
     SETostatniPoziomFiltracjiFoto = [];
     testActiveSlides = [];
     activeSlides = [];
+    showAllSlides()
+    hideAllSlides()
     allItems.forEach(i=>{
-    i.classList.remove("display-none");
-    i.classList.add("display-none");
     if(i.classList.contains(`${selectedOption}`)) {
        i.classList.remove("display-none");
        testActiveSlides.push(i)
        filtracjaKategoriiPoInpucie()
     }
     activeSlides = testActiveSlides;
-    startFromFirstSlide()
-       return ostatniPoziomFiltracjiFoto;
+    startFromFirstSlide(currentSlide)
+    activateCurrentDot(currentSlide)
+    return ostatniPoziomFiltracjiFoto;
 })
 }
 
 const filtracjaKategoriiPoInpucie = function(){
     odfiltrowaneKlucze = [];
     SETwidoczneKategorie=[];
-    allCategorys.forEach(c=>{
-        c.classList.add("display-none");      
+    widoczneKategorie = [];
+    hideAllCategories()
+    allCategorys.forEach(c=>{     
         catArr.forEach(el => {
-            testActiveSlides.forEach(ti=>{
-            if (ti.classList.contains(`${el}`)) {
-                odfiltrowaneKlucze.push(el)
-                SETostatniPoziomFiltracjiFoto.push(ti)
-                ostatniPoziomFiltracjiFoto = [...new Set(SETostatniPoziomFiltracjiFoto)]
-            }
+            activeSlides.forEach(ti=>{
+                if (ti.classList.contains(`${el}`)) {
+                    odfiltrowaneKlucze.push(el)
+                    SETostatniPoziomFiltracjiFoto.push(ti)
+                    ostatniPoziomFiltracjiFoto = [...new Set(SETostatniPoziomFiltracjiFoto)]
+                }
             })  
         })
         aktywneKlucze = [...new Set(odfiltrowaneKlucze)];
@@ -80,79 +129,79 @@ const filtracjaKategoriiPoInpucie = function(){
 
 const filtracjaPoWybranuiKategorii = function(){
     widoczneKategorie.forEach(c=>{
-        c.addEventListener('click', function() {            
-            allItems.forEach(i=>{
-                i.classList.add('display-none')
-            })
-            allCategorys.forEach(cat=>{
-                cat.classList.remove('category-item__On-Click');
-            })
+        c.addEventListener('click', function(event) {   
+            event.preventDefault()
+            event.stopPropagation()
+            hideAllSlides()
+            hideAllActiveCategories()
             c.classList.add('category-item__On-Click');
-            SETtestActiveSlides = []
             wybranaGrupaFoto = []
             activeSlides = []
+            currentSlide = 0
             aktywneKlucze.forEach(activeC=>{              
                 if (c.classList.contains(`${activeC}`)) {
                    ostatniPoziomFiltracjiFoto.forEach(f=>{
                             f.classList.add('display-none')
                         if(f.classList.contains(`${activeC}`)){
                             f.classList.remove('display-none')
-                            activeSlides.push(f)
+                            activeSlides.push(f)                            
                         }
                     })                                     
                 }
             })
             creatDotsForActiveSlides(activeSlides)
-            startFromFirstSlide()
-            return activeSlides            
+            activateCurrentDot(currentSlide)
+            startFromFirstSlide(currentSlide)
+            return currentSlide            
         })
     })    
 }
 
 const wubranyInPutWszystko = function(selectedOption){
     if (selectedOption === "all") {
-        activeSlides = []
-        document.querySelectorAll('div.prod').forEach(s => {
-            s.classList.remove('display-none')
-            activeSlides.push(s)
-        })
+        activeSlides = [...allItems]
+        showActiveSlides()
         document.querySelectorAll('div.category-item').forEach(c => {
             c.classList.remove('display-none');
             c.classList.remove('category-item__On-Click')
         })
         creatDotsForActiveSlides(activeSlides)
+        startFromFirstSlide()
+        clickNaKategorie()
         return activeSlides
     }
 }
 
 //  filtracja slidów poza / przed użyciem InPut-a
-const clickNaKategorie = function(){
+const clickNaKategorie = function(){    
     allCategorys.forEach(c => {
-        c.addEventListener('click', function(){
+        c.addEventListener('click', function(event){
+            event.preventDefault()
+            event.stopPropagation()
+            currentSlide = 0
             activeSlides = [];
-            allCategorys.forEach(cat=>{
-                cat.classList.remove('category-item__On-Click');
-            })
+            aktywneKlucze = [];
+            hideAllActiveCategories()
             c.classList.add('category-item__On-Click')
-            allItems.forEach(i=>{
-                    i.classList.add('display-none')
-                })
-            catArr.forEach(key => {                   
+            hideAllSlides()
+            catArr.forEach(key => {
                 if(c.classList.contains(`${key}`)){
-                    allItems.forEach(i=>{
-                        if(i.classList.contains(`${key}`)){
-                            i.classList.remove('display-none')
-                            activeSlides.push(i)
-                        }
-                    })
+                    aktywneKlucze.push(key)
                 }
             })
-            startFromFirstSlide()
+            allItems.forEach(i=>{
+                if(i.classList.contains(`${[...aktywneKlucze]}`)){
+                    activeSlides.push(i)
+                }
+            })
+            showActiveSlides()
+            startFromFirstSlide(activeSlides)
             creatDotsForActiveSlides(activeSlides)
             return activeSlides
         })
     })
 }
+
 clickNaKategorie()
 
 //  KONIC FILTRA
@@ -160,27 +209,41 @@ clickNaKategorie()
 
 // SLIDER
 
-let currentSlide = 0;
+const removeDotActiveFromAll = function(){
+    document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+}
+
+const addDotActiveForCurrentDot = function(index){
+    document.querySelector(`.dots__dot[data-slide ="${index}"]`).classList.add('dots__dot--active')
+}
 
 const creatDotsForActiveSlides = function(){
         dotContainer.innerHTML = " ";
         activeSlides.forEach(function(_, index){
         dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${index}"></button>`)
     })
+    addDotActiveForCurrentDot(currentSlide)
 }
 
 creatDotsForActiveSlides()
 
 const activateCurrentDot = function(currentSlide) {
-    document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
-    document.querySelector(`.dots__dot[data-slide ="${currentSlide}"]`).classList.add('dots__dot--active')
+    if(currentSlide >= 0){
+        removeDotActiveFromAll()
+        addDotActiveForCurrentDot(currentSlide)   
+    } else {
+        dotContainer.innerHTML = " ";
+    }
 }
 
 const startFromFirstSlide = function(){
     activeSlides.forEach((s, index) => {
+        if(s.style.transform === `translateX(0%)`){
+            s.style.transform = `translateX(${index * 100}%)`
+        }
         if(index === 0) {
             s.style.transform = `translateX(${index}%)`
-        }
+        }        
     })
 }
 
@@ -195,7 +258,6 @@ const nextSlide = function(){
         currentSlide++;
     }
     moveToSlide(currentSlide);
-
    activateCurrentDot(currentSlide)
 }
 
@@ -211,9 +273,17 @@ const previousSlide = function(){
 
 moveToSlide(0);
 
-btnRight.addEventListener('click', nextSlide);
+btnRight.addEventListener('click', function(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    nextSlide()
+});
 
-btnLeft.addEventListener('click', previousSlide);
+btnLeft.addEventListener('click', function(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    previousSlide()
+});
 
 document.addEventListener('keydown', function(e) {
     if (e.key === 'ArrowRight') nextSlide();
